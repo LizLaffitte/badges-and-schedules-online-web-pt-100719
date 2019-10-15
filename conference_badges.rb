2bdd57e13eb9b96f62b579bf41b84134fcfd1f18
Liz Laffitte <liz.kossakowski@gmail.com>
2019-10-15 21:54:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,27 +1,25 @@
-# Write your code here.
 def badge_maker(name)
- "Hello, my name is #{name}."
-end 
+	"Hello, my name is #{name}."
+end
 
-def batch_badge_creator(name_array)
-  badge_messages = []
-  name_array.each do |name|
-    badge_messages.push(badge_maker(name))
-  end
-  badge_messages
+def batch_badge_creator(attendees)
+	group_badge_messages = []
+	attendees.each do |attendee|
+		group_badge_messages << badge_maker(attendee)
+	end
+	group_badge_messages
 end
 
 def assign_rooms(attendees)
-  room_assignments = []
-  attendees.each_with_index do |attendee, index|
-    room = index + 1
-    room_assignments.push("Hello, #{attendee}! You'll be assigned to room #{room}!")
-  end 
-  room_assignments
-end 
+	room_assignments = []
+	attendees.each_with_index do |attendee, index|
+		room_num = (index + 1)
+		room_assignments << "Hello, #{attendee}! You'll be assigned to room #{room_num}!"
+	end
+	room_assignments
+end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each {|attendee| puts attendee}
- assign_rooms(attendees).each {|attendee| puts attendee}
-end 
-
+	batch_badge_creator(attendees).each {|attendee| puts attendee}
+	assign_rooms(attendees).each {|attendee| puts attendee}
+end
